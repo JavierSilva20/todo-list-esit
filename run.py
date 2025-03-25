1527237d1ce39d3848b6e34fc6f687cfe9889197
JavierSilva20 <168240712+JavierSilva20@users.noreply.github.com>
2025-03-25 17:31:00 -0600

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from todor import create_app
 # Validación de la función create_app
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run()
